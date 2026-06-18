4d7896ef2a791a8fd66c56bd37cecbaeb7e28dc4
Kylie Wang <yuxinwa2@andrew.cmu.edu>
2026-06-17 18:31:47 -0700

    Initial commit: Yjs server setup

---
+++ server.js
@@ -0,0 +1,21 @@
+const WebSocket = require('ws');
+const http = require('http');
+const { setupWSConnection } = require('y-websocket/bin/utils');
+
+// Create a basic HTTP server so we can check if it's alive in a browser
+const server = http.createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end('Yjs WebSocket server is running perfectly.');
+});
+
+// Attach the WebSocket server to the HTTP server
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (conn, req) => {
+    setupWSConnection(conn, req);
+});
+
+const PORT = process.env.PORT || 1234;
+server.listen(PORT, () => {
+    console.log(`WebSocket Server running on port ${PORT}`);
+});
